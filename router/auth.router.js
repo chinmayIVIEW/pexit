d6493684b35855a3a0a9b44a9574269a3f9bd999
chinmayIVIEW <chinmay.dora@iviewlabs.net>
2022-01-05 09:53:51 +0530

    new pexit repo

---
+++ router/auth.router.js
@@ -0,0 +1,11 @@
+const router = require("express").Router()
+const {delete_user,Signup,Signin} = require("../controller/auth.controller")
+
+
+
+router.get('/profile/delete-user/:id',delete_user)
+router.post('/signup',Signup)
+router.post('/signin',Signin)
+
+
+module.exports = router
